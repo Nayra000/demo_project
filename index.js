b171d0a4fb26afebf7d7a5b54c2400b258a273bf
nayra000 <youssefnayra26@gmail.com>
2026-07-06 20:25:52 +0300

    Remove duplicate functions and simplify complex logic for cleaner code

--- index.js
+++ index.js
@@ -8,59 +8,6 @@ const app = express();
 const port = process.env.PORT ;
 const mongoUri = process.env.MONGO_URI ;
 
-
-
-function duplicate1() {
-  console.log("line1");
-  console.log("line2");
-  console.log("line3");
-  console.log("line4");
-  console.log("line5");
-  console.log("line6");
-  console.log("line7");
-  console.log("line8");
-  console.log("line9");
-  console.log("line10");
-}
-
-function duplicate2() {
-  console.log("line1");
-  console.log("line2");
-  console.log("line3");
-  console.log("line4");
-  console.log("line5");
-  console.log("line6");
-  console.log("line7");
-  console.log("line8");
-  console.log("line9");
-  console.log("line10");
-}
-
-
-function complex(a, b, c, d) {
-  if (a) {
-    if (b) {
-      if (c) {
-        if (d) {
-          console.log(1);
-        } else {
-          console.log(2);
-        }
-      } else {
-        console.log(3);
-      }
-    } else {
-      console.log(4);
-    }
-  } else {
-    console.log(5);
-  }
-}
-
-let value = null;
-
-console.log(value.length);
-
 app.get('/backend-apis', (req, res) => {
   res.send('Hello from demo app');
 });
@@ -81,8 +28,3 @@ async function start() {
 }
 
 start();
-
-duplicate1();
-duplicate2();
-
-complex(true, true, true, false);
